3b7420126a642617b7d11266385abfaf5b44e108
Matthias Drescher <kingxelor@live.com>
2016-08-24 21:38:35 +0200

    Initial Checkin - empty window and a few projects

---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using QCF.SingleGameVisualization.ViewModels.MainWindow;
+
+namespace QCF.SingleGameVisualization
+{
+	public partial class App
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var mainWindowViewModel = new MainWindowViewModel();
+
+			var mainWindow = new MainWindow
+			{
+				DataContext = mainWindowViewModel
+			};
+			
+			mainWindow.ShowDialog();
+		}
+	}
+}
---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/ViewModels/MainWindow/IMainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace QCF.SingleGameVisualization.ViewModels.MainWindow
+{
+	internal interface IMainWindowViewModel
+	{
+	}
+
+	internal class MainWindowViewModel : IMainWindowViewModel
+	{
+		
+	}
+
+	internal class MainWindowViewModelSampleData : IMainWindowViewModel
+	{
+		
+	}
+}
