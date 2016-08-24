b078fb7a0a375b3dbb5eb42aa11163199bb42627
Matthias Drescher <kingxelor@live.com>
2016-08-24 22:43:27 +0200

    mainWindowViewModel extended by viewModel-base classes

---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using QCF.UiTools.WpfTools.ViewModelBase;
+
+namespace QCF.SingleGameVisualization.ViewModels.MainWindow
+{
+	internal class MainWindowViewModel : ViewModel, IMainWindowViewModel
+	{
+		protected override void CleanUp() {	}
+		public override event PropertyChangedEventHandler PropertyChanged;
+	}
+}
---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/ViewModels/MainWindow/MainWindowViewModelSampleData.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel;
+
+namespace QCF.SingleGameVisualization.ViewModels.MainWindow
+{
+	internal class MainWindowViewModelSampleData : IMainWindowViewModel
+	{
+		public void Dispose () { }
+		public event PropertyChangedEventHandler PropertyChanged;		
+	}
+}
