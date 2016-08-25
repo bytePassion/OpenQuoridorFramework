9f526fa935e334c8ead043131f0736bc1abac7ca
Matthias Drescher <kingxelor@gmail.com>
2016-08-25 15:13:03 +0200

    simplyfied wall

--- QuoridorContestFramework/QCF.GameEngine/GameElements/Wall.cs
+++ QuoridorContestFramework/QCF.GameEngine/GameElements/Wall.cs
@@ -4,20 +4,13 @@ namespace QCF.GameEngine.GameElements
 {
 	public class Wall
 	{
-		public Wall(XField left, XField right, YField top, YField bottom, WallOrientation orientation)
+		public Wall(FieldCoordinate topLeft, WallOrientation orientation)
 		{
-			Left = left;
-			Right = right;
-			Top = top;
-			Bottom = bottom;
 			Orientation = orientation;
+			TopLeft = topLeft;
 		}
 
-		public XField Left   { get; }
-		public XField Right  { get; }
-		public YField Top    { get; }
-		public YField Bottom { get; }
-
+		public FieldCoordinate TopLeft { get; }		
 		public WallOrientation Orientation { get; }
 	}
 }
