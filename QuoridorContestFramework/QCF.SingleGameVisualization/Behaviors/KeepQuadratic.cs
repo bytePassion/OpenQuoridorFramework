22d9b4877b1d1a3375a642c2cc7edc423cd5e228
Matthias Drescher <kingxelor@gamil.com>
2016-08-27 15:58:11 +0200

    added behavior to keep playing area quadratic .... but it's not working

---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/Behaviors/KeepQuadratic.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+using QCF.UiTools.Utils;
+
+namespace QCF.SingleGameVisualization.Behaviors
+{
+	internal class KeepQuadratic : Behavior<FrameworkElement>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			AssociatedObject.SizeChanged += OnSizeChanged;
+		}		
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+
+			AssociatedObject.SizeChanged += OnSizeChanged;
+		}
+
+		private void OnSizeChanged (object sender, SizeChangedEventArgs sizeChangedEventArgs)
+		{
+			var currentSize = sizeChangedEventArgs.NewSize;
+
+			if (!GeometryLibUtils.DoubleEquals(currentSize.Height, currentSize.Width))
+			{
+				var newSize = currentSize.Height < currentSize.Width 
+									? new Size(currentSize.Height, currentSize.Height) 
+									: new Size(currentSize.Width,  currentSize.Width);
+
+				((FrameworkElement) sender).Height = newSize.Height;
+				((FrameworkElement) sender).Width  = newSize.Width;
+			}
+		}
+	}
+}
