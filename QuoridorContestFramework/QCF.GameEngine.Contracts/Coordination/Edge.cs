263cff6cde917b640397b9e4d82ffd1520a77171
Alexander Horn <alexomanie@gmail.com>
2016-08-28 15:06:42 +0200

    Introduce Edge element and initial checkin of first graph draft.

---
+++ QuoridorContestFramework/QCF.GameEngine.Contracts/Coordination/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QCF.GameEngine.Contracts.Coordination
+{
+    public class Edge
+    {
+        public FieldCoordinate Node1 { get; }
+        public FieldCoordinate Node2 { get; }
+
+        public Edge(FieldCoordinate node1, FieldCoordinate node2)
+        {
+            Node1 = node1;
+            Node2 = node2;
+        }
+    }
+}
---
+++ QuoridorContestFramework/QCF.GameEngine/GameGraph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QCF.GameEngine.Contracts.Coordination;
+
+namespace QCF.GameEngine
+{
+    //non directed graph with edges added in both directions 
+    public class GameGraph
+    {
+        private readonly Dictionary<FieldCoordinate, List<FieldCoordinate>> graph = new Dictionary<FieldCoordinate, List<FieldCoordinate>>();
+
+        private void AddEdge(Edge edge)
+        {
+            AddNode(edge.Node1);
+            AddNode(edge.Node2);
+
+            AddNeighbor(edge);
+        }
+
+        private void AddNeighbor(Edge edge)
+        {
+            if(!graph[edge.Node1].Contains(edge.Node2))
+                graph[edge.Node1].Add(edge.Node2);
+
+            if (!graph[edge.Node2].Contains(edge.Node1))
+                graph[edge.Node2].Add(edge.Node1);
+        }
+
+        private void AddNode(FieldCoordinate node)
+        {
+            if(!graph.ContainsKey(node))
+                graph.Add(node, new List<FieldCoordinate>());
+        }
+
+        public void RemoveEdge(Edge edge)
+        {
+            
+        }
+
+        public bool ValidateMove(Edge edge)
+        {
+            //nodes of edge still connected?
+            if (!graph[edge.Node1].Contains(edge.Node2))
+                return false;
+
+            //search if open path still available
+
+            return true;
+        }
+
+        private bool IsExitPathAvailable(IEnumerable<FieldCoordinate> exitNodes)
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            foreach (var node in graph)
+            {
+                var elements = "";
+                node.Value.ForEach(value => elements += value + ",");
+                builder.AppendLine($"{node.Key}: {elements}");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
