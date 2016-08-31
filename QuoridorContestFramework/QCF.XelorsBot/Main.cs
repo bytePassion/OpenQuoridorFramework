9c9fb9d6d285140efacf5e564c0c20cd7bf250d9
Matthias Drescher <kingxelor@gmail.com>
2016-08-31 16:16:25 +0200

    added sceloton for xelor's bot

---
+++ QuoridorContestFramework/QCF.XelorsBot/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QCF.Contest.Contracts;
+using QCF.Contest.Contracts.GameElements;
+using QCF.Contest.Contracts.Moves;
+
+namespace QCF.XelorsBot
+{
+    public class Main : IQuoridorBot
+    {
+	    public event Action<Move> NextMoveAvailable;
+	    public event Action<string> DebugMessageAvailable;
+
+	    public void Init(Player yourPlayer)
+	    {
+		    throw new NotImplementedException();
+	    }
+
+	    public void DoMove(BoardState currentState)
+	    {
+		    throw new NotImplementedException();
+	    }
+    }
+}
