145f764cd828e79b7936cbc8b193c3051861f7c2
Matthias Drescher <kingxelor@gmail.com>
2016-08-31 14:15:39 +0200

    slight code-cleanup

---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/Computation/ComputeFigureLeftPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows;
+using QCF.Contest.Contracts.GameElements;
+using QCF.Tools.WpfTools.ConverterBase;
+
+namespace QCF.SingleGameVisualization.Computation
+{
+	internal class ComputeFigureLeftPosition : GenericTwoToOneValueConverter<PlayerState, Size, double>
+	{
+		protected override double Convert(PlayerState currentPlayerState, Size currentBoadSize, CultureInfo culture)
+		{
+			var cellWidth = currentBoadSize.Width / 11.4;
+						
+			return (double)currentPlayerState.Position.XCoord * 1.3 * cellWidth + 1;
+		}
+	}
+}
---
+++ QuoridorContestFramework/QCF.SingleGameVisualization/Computation/ComputeWallHeight.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using QCF.Contest.Contracts.Coordination;
+using QCF.Contest.Contracts.GameElements;
+using QCF.Tools.WpfTools.ConverterBase;
+
+namespace QCF.SingleGameVisualization.Computation
+{
+	internal class ComputeWallHeight : GenericTwoToOneValueConverter<Wall, Size, double>
+	{
+		protected override double Convert (Wall wall, Size currentBoadSize, CultureInfo culture)
+		{
+			var cellHeight = currentBoadSize.Height / 11.4;
+
+			return wall.Orientation == WallOrientation.Horizontal
+				? 0.3 * cellHeight
+				: 2.3 * cellHeight;			
+		}
+	}
+}
