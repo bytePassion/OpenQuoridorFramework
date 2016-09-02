2b4305b0c50fea5a6f77b3fb6884ed4be4c2a7b1
Matthias Drescher <kingxelor@gmail.com>
2016-09-02 13:45:16 +0200

    moved FieldCoordinateExtensions to own file

---
+++ QuoridorContestFramework/QCF.GameEngine/Analysis/FieldCoordinateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using QCF.Contest.Contracts.Coordination;
+
+namespace QCF.GameEngine.Analysis
+{
+	internal static class FieldCoordinateExtensions
+	{
+		public static bool ExistsLeft  (this FieldCoordinate coord) => coord.XCoord != XField.A;
+		public static bool ExistsRight (this FieldCoordinate coord) => coord.XCoord != XField.I;
+		public static bool ExistsTop   (this FieldCoordinate coord) => coord.YCoord != YField.Nine;
+		public static bool ExistsBottom(this FieldCoordinate coord) => coord.YCoord != YField.One;
+
+		public static FieldCoordinate GetLeft  (this FieldCoordinate coord) => new FieldCoordinate(coord.XCoord - 1, coord.YCoord    );
+		public static FieldCoordinate GetRight (this FieldCoordinate coord) => new FieldCoordinate(coord.XCoord + 1, coord.YCoord    );
+		public static FieldCoordinate GetTop   (this FieldCoordinate coord) => new FieldCoordinate(coord.XCoord    , coord.YCoord - 1);
+		public static FieldCoordinate GetBottom(this FieldCoordinate coord) => new FieldCoordinate(coord.XCoord    , coord.YCoord + 1);
+	}
+}
