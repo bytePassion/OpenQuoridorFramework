b4ee27b34076b7bf8a33c85ef0cb0d32e17e42eb
Matthias Drescher <kingxelor@gmail.com>
2016-09-02 14:45:02 +0200

    figure can be moved by click

--- QuoridorContestFramework/QCF.GameEngine/Analysis/HumanPlayerAnalysis.cs
+++ QuoridorContestFramework/QCF.GameEngine/Analysis/HumanPlayerAnalysis.cs
@@ -9,9 +9,26 @@ namespace QCF.GameEngine.Analysis
 	{
 		private readonly BoardState boardState;
 
+		private readonly IEnumerable<FieldCoordinate> possibleMoves;
+
 		public HumanPlayerAnalysis(BoardState boardState)
 		{
 			this.boardState = boardState;
+
+			var gameGraph = new GameGraph().InitGraph()
+										   .ApplyWalls(boardState.PlacedWalls);
+
+			possibleMoves = new List<FieldCoordinate>();
+
+			if (boardState.CurrentMover.PlayerType == PlayerType.BottomPlayer)
+			{
+				var node = gameGraph.GetNodeForCoordinate(boardState.BottomPlayer.Position);
+
+				foreach (var nodeNeighbor in node.Neighbors)
+				{
+					((List<FieldCoordinate>)possibleMoves).Add(nodeNeighbor.Coordinate);
+				}
+			}			
 		}
 
 		public IEnumerable<Wall> GetPossibleWalls()
@@ -20,10 +37,6 @@ namespace QCF.GameEngine.Analysis
 			return new List<Wall>();
 		}
 
-		public IEnumerable<FieldCoordinate> GetPossibleMoves()
-		{
-			// TODO: implementation
-			return new List<FieldCoordinate>();
-		}
+		public IEnumerable<FieldCoordinate> GetPossibleMoves() => possibleMoves;
 	}
 }
