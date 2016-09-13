22d1c599962d315c18b65d087a30fda6490d03cd
Alexander Horn <alexomanie@gmail.com>
2016-09-13 22:10:51 +0200

    first layout modifications on UI

--- OpenQuoridorFramework/OQF.PlayerVsBot/ViewModels/MainWindow/MainWindowViewModel.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -286,7 +286,7 @@ namespace OQF.PlayerVsBot.ViewModels.MainWindow
 			if (result.HasValue)
 				if (result.Value)
 					DllPathInput = dialog.FileName;
-		}
+        }
 
 		private void DoStart()
 		{
