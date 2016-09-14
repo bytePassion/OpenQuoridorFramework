58339220654cf5035732a710fa891c4e8f937c2f
Alexander Horn <alexomanie@gmail.com>
2016-09-14 15:08:15 +0200

    Continue to restyle UI

--- OpenQuoridorFramework/OQF.PlayerVsBot/Global/Constants.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot/Global/Constants.cs
@@ -7,8 +7,8 @@ namespace OQF.PlayerVsBot.Global
 		public static readonly SolidColorBrush TopPlayerActiveColor     = new SolidColorBrush(Colors.Red);
 		public static readonly SolidColorBrush BottomPlayerActiveColor  = new SolidColorBrush(Colors.GreenYellow);
 		public static readonly SolidColorBrush PlayerInactiveColor      = new SolidColorBrush(Colors.DarkGray);
-		public static readonly SolidColorBrush FieldBackgroundColor     = new SolidColorBrush(Colors.Aqua);
-		public static readonly SolidColorBrush FieldColor               = new SolidColorBrush(Colors.White);
+		public static readonly SolidColorBrush FieldBackgroundColor     = new SolidColorBrush(Color.FromRgb(121, 93, 86));
+		public static readonly SolidColorBrush FieldColor               = new SolidColorBrush(Color.FromRgb(97, 64, 56));
 		public static readonly SolidColorBrush FrameColor		        = new SolidColorBrush(Colors.Black);
 		public static readonly SolidColorBrush WallColor                = new SolidColorBrush(Colors.DeepPink);
 		public static readonly SolidColorBrush PossibleMoveFieldColor   = new SolidColorBrush(Colors.Orange);
