2320a1a0e5a32512eb42ed459a2e1cbac2ffc4e2
Matthias Drescher <kingxelor@gmail.com>
2016-09-20 14:24:21 +0200

    replaced Language-Info by more general OpenQuoridorFrameworkInfo

---
+++ OpenQuoridorFramework/OQF.Visualization.Resources/ApplicationInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace OQF.Visualization.Resources
+{
+	public class ApplicationInfo
+	{
+		public ApplicationInfo(string name, string version, string licence, 
+							   IEnumerable<string> developers, IEnumerable<string> thirdPartyItems)
+		{
+			Name = name;
+			Version = version;
+			Licence = licence;
+			Developers = developers;
+			ThirdPartyItems = thirdPartyItems;
+		}
+
+		public IEnumerable<string> Developers { get; }
+		public IEnumerable<string> ThirdPartyItems { get; }
+
+		public string Name    { get; }
+		public string Version { get; }
+		public string Licence { get; }
+	}
+}
---
+++ OpenQuoridorFramework/OQF.Visualization.Resources/OpenQuoridorFrameworkInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace OQF.Visualization.Resources
+{
+	public static class OpenQuoridorFrameworkInfo
+	{
+		public static readonly IEnumerable<string> AvailableLanguageCodes = new List<string>
+		{
+			"de",
+			"en"
+		};
+		
+		public static readonly IEnumerable<string> ActiveDevelopers = new List<string>
+		{
+			"Matthias Drescher (matthias.drescher@bytePassion.de)",
+			"Alexander Horn (alex.horn@bytePassion.de)"
+		};
+		
+		public const string Licence = "Apache 2.0";
+	
+		public static class Applications
+		{
+			public static class PlayerVsBot
+			{
+				private const string Name    = "Player vs. Bot";
+				private const string Version = "1.0.0.0";
+				private static readonly IEnumerable<string> ThirdPartyItems = new List<string>
+				{
+					"Flagicon from www.iconDrawer.com"
+				};
+
+				public static readonly ApplicationInfo Info = new ApplicationInfo(Name, Version, Licence, ActiveDevelopers, ThirdPartyItems);
+			}
+
+			public static class ReplayViewer
+			{
+				public const string Name = "Replay Viewer";
+				public const string Version = "1.0.0.0";
+				public static readonly IEnumerable<string> ThirdPartyItems = new List<string>
+				{
+					"Flagicon from www.iconDrawer.com"
+				};
+
+				public static readonly ApplicationInfo Info = new ApplicationInfo(Name, Version, Licence, ActiveDevelopers, ThirdPartyItems);
+			}			
+		}
+	}
+}
