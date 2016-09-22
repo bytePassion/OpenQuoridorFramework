3610c7385ed2d868f18ce3eea821e9de3197c79f
Matthias Drescher <kingxelor@gmail.com>
2016-09-22 10:14:13 +0200

    replaced Converter to find bug

--- OpenQuoridorFramework/OQF.PlayerVsBot/Computation/ComputeFigureWidth.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot/Computation/ComputeFigureWidth.cs
@@ -1,17 +1,37 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
-using Lib.Wpf.ConverterBase;
+using System.Windows.Data;
 using OQF.Bot.Contracts.GameElements;
+using OQF.PlayerVsBot.Global;
 
 namespace OQF.PlayerVsBot.Computation
 {
-	internal class ComputeFigureWidth : GenericTwoToOneValueConverter<PlayerState, Size, double>
+	//	internal class ComputeFigureWidth : GenericTwoToOneValueConverter<PlayerState, Size, double>
+	//	{
+	//		protected override double Convert (PlayerState currentPlayerState, Size currentBoadSize, CultureInfo culture)
+	//		{
+	//			var cellWidth = currentBoadSize.Width / 11.4;
+	//
+	//			return cellWidth - 4;
+	//		}
+	//	}
+
+	internal class ComputeFigureWidth : IMultiValueConverter
 	{
-		protected override double Convert (PlayerState currentPlayerState, Size currentBoadSize, CultureInfo culture)
+		public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
-			var cellWidth = currentBoadSize.Width / 11.4;
+			var currentPlayerState = (PlayerState) values[0];
+			var currentBoardSize = values[1] as Size? ?? Constants.SizeFallBackValue;
+
+			var cellWidth = currentBoardSize.Width / 11.4;
 
 			return cellWidth - 4;
+		}
+
+		public object[] ConvertBack (object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
--- OpenQuoridorFramework/OQF.PlayerVsBot/Computation/ComputeWallWidth.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot/Computation/ComputeWallWidth.cs
@@ -1,20 +1,42 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
-using Lib.Wpf.ConverterBase;
+using System.Windows.Data;
 using OQF.Bot.Contracts.Coordination;
 using OQF.Bot.Contracts.GameElements;
+using OQF.PlayerVsBot.Global;
 
 namespace OQF.PlayerVsBot.Computation
 {
-	internal class ComputeWallWidth : GenericTwoToOneValueConverter<Wall, Size, double>
+	//	internal class ComputeWallWidth : GenericTwoToOneValueConverter<Wall, Size, double>
+	//	{
+	//		protected override double Convert (Wall wall, Size currentBoadSize, CultureInfo culture)
+	//		{
+	//			var cellWidth = currentBoadSize.Width / 11.4;
+	//
+	//			return wall.Orientation == WallOrientation.Horizontal
+	//				? 2.3 * cellWidth
+	//				: 0.3 * cellWidth;
+	//		}
+	//	}
+
+	internal class ComputeWallWidth : IMultiValueConverter
 	{
-		protected override double Convert (Wall wall, Size currentBoadSize, CultureInfo culture)
+		public object Convert (object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
-			var cellWidth = currentBoadSize.Width / 11.4;
+			var wall = (Wall) values[0];
+			var currentBoardSize = values[1] as Size? ?? Constants.SizeFallBackValue;
+
+			var cellWidth = currentBoardSize.Width / 11.4;
 
 			return wall.Orientation == WallOrientation.Horizontal
 				? 2.3 * cellWidth
 				: 0.3 * cellWidth;
+		}
+
+		public object[] ConvertBack (object value, Type[] targetTypes, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
