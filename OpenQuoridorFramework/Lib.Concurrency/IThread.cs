f5fa66a55b89735ae6a03f83752c839d44dd26bf
Matthias Drescher <kingxelor@gamil.com>
2016-09-27 19:40:53 +0200

    added exampleThread as comment to IThread

--- OpenQuoridorFramework/Lib.Concurrency/IThread.cs
+++ OpenQuoridorFramework/Lib.Concurrency/IThread.cs
@@ -7,4 +7,39 @@
 		
 		bool IsRunning { get; }
 	}
+
+	
+	/*
+	 
+	public class ExampleThread : IThread
+	{
+		private volatile bool stopRunning;
+	
+		public ExampleThread ()
+		{						
+		}
+	
+		public void Run ()
+		{
+			IsRunning = true;			
+
+			while (!stopRunning)
+			{
+				// DO Stuff
+			}
+
+			IsRunning = false;			
+		}
+
+	
+
+		public void Stop ()
+		{
+			stopRunning = true;
+		}
+
+		public bool IsRunning { get; private set; }
+	}	 
+	 
+	 */
 }
