7908322880976c51742f30d4256ad7b2908b9488
Matthias Drescher <kingxelor@gmail.com>
2016-10-01 18:01:15 +0200

    added commandLineParser

--- OpenQuoridorFramework/OQF.PlayerVsBot/Global/Constants.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot/Global/Constants.cs
@@ -1,10 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Media;
-
-namespace OQF.PlayerVsBot.Global
+﻿namespace OQF.PlayerVsBot.Global
 {
 	internal static class Constants
-	{
+	{		
 		internal static class GameContraint
 		{
 			public const int BotThinkingTimeSeconds = 60;
