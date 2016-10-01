00b4d6051b601b3d3057e52774e166450c7fdc14
Alexander Horn <alexomanie@gmail.com>
2016-10-01 12:18:20 +0200

    ﻿
    add pages property to interface

--- OpenQuoridorFramework/OQF.Visualization.Common/Info/InfoWindow/ViewModel/IInfoWindowViewModel.cs
+++ OpenQuoridorFramework/OQF.Visualization.Common/Info/InfoWindow/ViewModel/IInfoWindowViewModel.cs
@@ -1,15 +1,16 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Lib.Wpf.ViewModelBase;
+using OQF.Visualization.Common.Info.Pages.PageViewModels;
 using OQF.Visualization.Common.Language.LanguageSelection.ViewModel;
 
 namespace OQF.Visualization.Common.Info.InfoWindow.ViewModel
 {
-	internal interface IInfoWindowViewModel : IViewModel
-	{
-
-		ILanguageSelectionViewModel LanguageSelectionViewModel { get; }
-
-		ICommand CloseWindow { get; }
-		string CloseButtonCaption { get; }
-	}
+    internal interface IInfoWindowViewModel : IViewModel
+    {
+        ILanguageSelectionViewModel LanguageSelectionViewModel { get; }
+        ObservableCollection<IPage> Pages { get; }
+        ICommand CloseWindow { get; }
+        string CloseButtonCaption { get; }
+    }
 }
