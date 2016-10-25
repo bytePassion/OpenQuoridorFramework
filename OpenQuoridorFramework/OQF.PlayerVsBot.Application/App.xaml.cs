4105971b4051d54c1ad5445cce381716e59ebddd
Matthias Drescher <kingxelor@gmail.com>
2016-10-25 16:23:28 +0200

    playerVsBot: refactored composition root to seperate class

--- OpenQuoridorFramework/OQF.PlayerVsBot.Application/App.xaml.cs
+++ OpenQuoridorFramework/OQF.PlayerVsBot.Application/App.xaml.cs
@@ -1,84 +1,24 @@
-﻿using System.Globalization;
-using System.Windows;
-using OQF.CommonUiElements.Language.LanguageSelection.ViewModel;
-using OQF.GameEngine.Contracts.Factories;
-using OQF.GameEngine.Factories;
-using OQF.PlayerVsBot.Contracts.Settings;
-using OQF.PlayerVsBot.Visualization.Services;
-using OQF.PlayerVsBot.Visualization.ViewModels.Board;
-using OQF.PlayerVsBot.Visualization.ViewModels.BoardPlacement;
-using OQF.PlayerVsBot.Visualization.ViewModels.MainWindow;
-using OQF.PlayerVsBot.Visualization.Windows;
-using OQF.Utils;
+﻿using System.Windows;
+using Lib.Wpf;
 
 namespace OQF.PlayerVsBot.Application
 {
 	public partial class App
 	{
+		private IApplicationLifeCycle applicationLifeCycle;
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			base.OnExit(e);
+			applicationLifeCycle.CleanUp(e);
+		}
+
 		protected override void OnStartup (StartupEventArgs e)
 		{
 			base.OnStartup(e);
-
-			var commandLineArguments = CommandLine.Parse(e.Args);
-
-			IGameFactory gameFactory = new GameFactory();
-			IGameService gameService = new GameService(gameFactory, commandLineArguments.DisableBotTimeout);
-			IApplicationSettingsRepository applicationSettingsRepository = new ApplicationSettingsRepository();
-
-			CultureManager.CurrentCulture = new CultureInfo(applicationSettingsRepository.SelectedLanguageCode);
-			CultureManager.CultureChanged += () =>
-			{
-				applicationSettingsRepository.SelectedLanguageCode = CultureManager.CurrentCulture.ToString();
-			};
-
-			var boardViewModel = new BoardViewModel(gameService);
-			var boardPlacementViewModel = new BoardPlacementViewModel(gameService, gameFactory);
-			var languageSelectionViewModel = new LanguageSelectionViewModel();
-			var progressFileVerifierFactory = new ProgressFileVerifierFactory();
-
-			var mainWindowViewModel = new MainWindowViewModel(boardViewModel,
-															  boardPlacementViewModel,
-															  languageSelectionViewModel,
-															  gameService,
-															  applicationSettingsRepository,
-															  progressFileVerifierFactory,
-															  commandLineArguments.DisableClosingDialogs);
-
-			if (!string.IsNullOrWhiteSpace(commandLineArguments.BotPath))
-			{
-				var dllPath = commandLineArguments.BotPath;
-				mainWindowViewModel.DllPathInput = dllPath;
-
-				if (string.IsNullOrWhiteSpace(commandLineArguments.ProgressFilePath))
-				{
-					if (mainWindowViewModel.Start.CanExecute(null))
-						mainWindowViewModel.Start.Execute(null);
-					else
-					{
-						MessageBox.Show("startup-error");
-						return;
-					}
-				}
-				else
-				{					
-					if (mainWindowViewModel.StartWithProgress.CanExecute(commandLineArguments.ProgressFilePath))
-						mainWindowViewModel.StartWithProgress.Execute(commandLineArguments.ProgressFilePath);
-					else
-					{
-						MessageBox.Show("startup-error");
-						return;
-					}
-				}				
-			}
-
-			var mainWindow = new MainWindow
-			{
-				DataContext = mainWindowViewModel
-			};
-
-			mainWindow.ShowDialog();
-
-			gameService.StopGame();
+			
+			applicationLifeCycle = new ApplicationLifeCycle();
+			applicationLifeCycle.BuildAndStart(e);
 		}
 	}
 }
---
+++ OpenQuoridorFramework/OQF.PlayerVsBot.Application/ApplicationLifeCycle.cs
@@ -0,0 +1,88 @@
+﻿using System.Globalization;
+using System.Windows;
+using Lib.Wpf;
+using OQF.CommonUiElements.Language.LanguageSelection.ViewModel;
+using OQF.GameEngine.Contracts.Factories;
+using OQF.GameEngine.Factories;
+using OQF.PlayerVsBot.Contracts.Settings;
+using OQF.PlayerVsBot.Visualization.Services;
+using OQF.PlayerVsBot.Visualization.ViewModels.Board;
+using OQF.PlayerVsBot.Visualization.ViewModels.BoardPlacement;
+using OQF.PlayerVsBot.Visualization.ViewModels.MainWindow;
+using OQF.PlayerVsBot.Visualization.Windows;
+using OQF.Utils;
+
+namespace OQF.PlayerVsBot.Application
+{
+	internal class ApplicationLifeCycle : IApplicationLifeCycle
+	{
+		private IGameService gameService;
+
+		public void BuildAndStart(StartupEventArgs startupEventArgs)
+		{
+			var commandLineArguments = CommandLine.Parse(startupEventArgs.Args);
+
+			IGameFactory gameFactory = new GameFactory();
+			gameService = new GameService(gameFactory, commandLineArguments.DisableBotTimeout);
+			IApplicationSettingsRepository applicationSettingsRepository = new ApplicationSettingsRepository();
+
+			CultureManager.CurrentCulture = new CultureInfo(applicationSettingsRepository.SelectedLanguageCode);
+			CultureManager.CultureChanged += () =>
+			{
+				applicationSettingsRepository.SelectedLanguageCode = CultureManager.CurrentCulture.ToString();
+			};
+
+			var boardViewModel = new BoardViewModel(gameService);
+			var boardPlacementViewModel = new BoardPlacementViewModel(gameService, gameFactory);
+			var languageSelectionViewModel = new LanguageSelectionViewModel();
+			var progressFileVerifierFactory = new ProgressFileVerifierFactory();
+
+			var mainWindowViewModel = new MainWindowViewModel(boardViewModel,
+															  boardPlacementViewModel,
+															  languageSelectionViewModel,
+															  gameService,
+															  applicationSettingsRepository,
+															  progressFileVerifierFactory,
+															  commandLineArguments.DisableClosingDialogs);
+
+			if (!string.IsNullOrWhiteSpace(commandLineArguments.BotPath))
+			{
+				var dllPath = commandLineArguments.BotPath;
+				mainWindowViewModel.DllPathInput = dllPath;
+
+				if (string.IsNullOrWhiteSpace(commandLineArguments.ProgressFilePath))
+				{
+					if (mainWindowViewModel.Start.CanExecute(null))
+						mainWindowViewModel.Start.Execute(null);
+					else
+					{
+						MessageBox.Show("startup-error");
+						return;
+					}
+				}
+				else
+				{
+					if (mainWindowViewModel.StartWithProgress.CanExecute(commandLineArguments.ProgressFilePath))
+						mainWindowViewModel.StartWithProgress.Execute(commandLineArguments.ProgressFilePath);
+					else
+					{
+						MessageBox.Show("startup-error");
+						return;
+					}
+				}
+			}
+
+			var mainWindow = new MainWindow
+			{
+				DataContext = mainWindowViewModel
+			};
+
+			mainWindow.Show();
+		}
+
+		public void CleanUp(ExitEventArgs exitEventArgs)
+		{
+			gameService.StopGame();
+		}
+	}
+}
