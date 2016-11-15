c4b843643b989d521a3547a765b814d99d511ea8
Matthias Drescher <kingxelor@gmail.com>
2016-11-15 14:09:58 +0100

    added BoardStateExtension to get all moves from a boardState

---
+++ OpenQuoridorFramework/OQF.Utils/BoardStateExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using OQF.Bot.Contracts.GameElements;
+using OQF.Bot.Contracts.Moves;
+
+namespace OQF.Utils
+{
+	public static class BoardStateExtensions
+	{
+		public static IEnumerable<Move> GetMoveList(this BoardState boardState)
+		{
+			var resultList = new List<Move>();
+
+			var currentBoardState = boardState;
+
+			while (currentBoardState != null)
+			{
+				if (currentBoardState.LastMove != null)
+					resultList.Add(currentBoardState.LastMove);
+
+				currentBoardState = currentBoardState.LastBoardState;
+			}
+
+			resultList.Reverse();
+
+			return resultList;			
+		}
+	}
+}
