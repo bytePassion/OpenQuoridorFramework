aa4ab72e0121f36e02d2756f883db9004c75b18e
Matthias Drescher <kingxelor@gmail.com>
2016-11-15 13:09:30 +0100

    added progressCoding-tools to OQF.Utils

---
+++ OpenQuoridorFramework/OQF.Utils/ProgressCodingUtils/ProgressCoding.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace OQF.Utils.ProgressCodingUtils
+{
+	public static class ProgressCoding
+	{
+
+		public static string ConvertToProgress (string progressAsString)
+		{ 
+			var progressAsCompressedString = progressAsString;
+
+			var progressAsNumber = BaseCoding.Decode(progressAsCompressedString);
+			var progressAsMoves = MoveListCoding.ConvertBigIntegerToMoveList(progressAsNumber);
+			var progressAsStringMoves = progressAsMoves.Select(move => move.ToString());
+			var progressTextList = CreateProgressText.FromMoveList(progressAsStringMoves.ToList());
+
+			var progressText = new StringBuilder();
+
+			foreach (var row in progressTextList)
+			{
+				progressText.Append(row + "\n");
+			}
+
+			return progressText.ToString();
+		}
+
+		public static string ConvertToString (string progress)
+		{
+			var progressText = progress;
+
+			var progressAsStringMoves = ParseProgressText.FromFileText(progressText);
+			var progressAsMoves = progressAsStringMoves.Select(MoveParser.GetMove);
+			var progressAsNumber = MoveListCoding.ConvertMoveListToBigInteger(progressAsMoves);
+			var progressAsCompressedString = BaseCoding.Encode(progressAsNumber);
+
+			return progressAsCompressedString;
+		}
+	}
+}
