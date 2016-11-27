b12267979306187f8ac19a2def98c2675a117849
Matthias Drescher <kingxelor@gmail.com>
2016-11-27 13:12:33 +0100

    added MessagingConstants

---
+++ OpenQuoridorFramework/OQF.Net.LanMessaging/MessagingConstants.cs
@@ -0,0 +1,18 @@
+﻿using OQF.Net.LanMessaging.AddressTypes;
+
+namespace OQF.Net.LanMessaging
+{
+	public static class MessagingConstants
+	{
+		public static class TcpIpPort
+		{
+			public static readonly IpPort PubSubPort   = new IpPort(6670);
+			public static readonly IpPort PushPullPort = new IpPort(6671);
+		}
+
+		public static class Timeout
+		{
+			public const uint StandardSendingTimeout = 2000;
+		}
+	}
+}
