d2565898a2d829b29fe3a07b83a3ec326463e573
Matthias Drescher <kingxelor@gmail.com>
2016-11-28 09:39:40 +0100

    first step to allow client to join a game

--- OpenQuoridorFramework/OQF.Net.LanMessaging/NetworkMessageBase/NetworkMessageType.cs
+++ OpenQuoridorFramework/OQF.Net.LanMessaging/NetworkMessageBase/NetworkMessageType.cs
@@ -4,6 +4,8 @@
 	{
 		ConnectToServerRequest,
 		ConnectToServerResponse,
+		JoinGameRequest,
+		JoinGameResponse,
 
 		CreateGameRequest,
 		
---
+++ OpenQuoridorFramework/OQF.Net.LanMessaging/NetworkMessages/RequestsAndResponses/JoidGameRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OQF.Net.LanMessaging.NetworkMessageBase;
+using OQF.Net.LanMessaging.Types;
+
+namespace OQF.Net.LanMessaging.NetworkMessages.RequestsAndResponses
+{
+	public class JoidGameRequest : NetworkMessageBase.NetworkMessageBase
+	{
+		public JoidGameRequest(ClientId clientId, NetworkGameId gameId) 
+			: base(NetworkMessageType.JoinGameRequest, clientId)
+		{
+			GameId = gameId;
+		}
+
+		public NetworkGameId GameId { get; }
+
+		public override string AsString()
+		{
+			return GameId.ToString();
+		}
+
+		public static JoidGameRequest Parse(ClientId clientId, string s)
+		{
+			return new JoidGameRequest(clientId, new NetworkGameId(Guid.Parse(s)));
+		}
+	}
+}
