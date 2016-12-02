a4982a277e63d4e8be612321461909a49c4de6f1
Matthias Drescher <kingxelor@gmail.com>
2016-12-02 21:30:04 +0100

    slight code cleanup

--- OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Views/NetworkView.xaml.cs
+++ OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Views/NetworkView.xaml.cs
@@ -1,24 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace OQF.Net.DesktopClient.Visualization.Views
+﻿namespace OQF.Net.DesktopClient.Visualization.Views
 {
-	/// <summary>
-	/// Interaction logic for NetworkView.xaml
-	/// </summary>
-	public partial class NetworkView : UserControl
+	public partial class NetworkView
 	{
 		public NetworkView ()
 		{
--- OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Views/RemotePlayerBar.xaml.cs
+++ OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Views/RemotePlayerBar.xaml.cs
@@ -1,24 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace OQF.Net.DesktopClient.Visualization.Views
+﻿namespace OQF.Net.DesktopClient.Visualization.Views
 {
-	/// <summary>
-	/// Interaction logic for RemotePlayerBar.xaml
-	/// </summary>
-	public partial class RemotePlayerBar : UserControl
+	public partial class RemotePlayerBar
 	{
 		public RemotePlayerBar ()
 		{
