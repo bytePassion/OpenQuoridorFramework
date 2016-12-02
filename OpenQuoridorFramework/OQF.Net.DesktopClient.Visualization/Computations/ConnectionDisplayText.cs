e220f6499703f5e59e653a592e6279616280a49c
Matthias Drescher <kingxelor@gmail.com>
2016-12-02 12:30:19 +0100

    Network.DesktopClient: improved ConnectionDisplayText

--- OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Computations/ConnectionDisplayText.cs
+++ OpenQuoridorFramework/OQF.Net.DesktopClient.Visualization/Computations/ConnectionDisplayText.cs
@@ -5,15 +5,27 @@ using OQF.Net.DesktopClient.Contracts;
 
 namespace OQF.Net.DesktopClient.Visualization.Computations
 {
-	internal class ConnectionDisplayText : GenericValueConverter<ConnectionStatus, string>
+	internal class ConnectionDisplayText : GenericTwoToOneValueConverter<ConnectionStatus, GameStatus, string>
 	{
-		protected override string Convert(ConnectionStatus value, CultureInfo culture)
+		protected override string Convert(ConnectionStatus connectionStatus, GameStatus gameStatus, CultureInfo culture)
 		{
-			switch (value)
+			switch (connectionStatus)
 			{
-				case ConnectionStatus.Connected:       { return "Connected"; }
-				case ConnectionStatus.NotConnected:    { return "Notconnected"; }
-				case ConnectionStatus.TryingToConnect: { return "Trying to connect ...."; }
+				case ConnectionStatus.Connected:
+				{
+					if (gameStatus == GameStatus.WaitingForOponend)
+						return "Connected - Waiting for opponend";
+					else
+						return "Connected";
+				}
+				case ConnectionStatus.NotConnected:
+				{
+					return "Not Connected";
+				}
+				case ConnectionStatus.TryingToConnect:
+				{
+					return "Trying to connect ....";
+				}
 			}
 
 			throw new ArgumentException();
