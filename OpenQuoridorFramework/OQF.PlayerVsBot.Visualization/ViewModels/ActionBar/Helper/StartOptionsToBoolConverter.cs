b8429e2aa909e9c3530ac7eda3910ab563063d96
Alexander Horn <alexomanie@gmail.com>
2016-12-13 12:49:58 +0100

    alternative layout proposal - actionbar

---
+++ OpenQuoridorFramework/OQF.PlayerVsBot.Visualization/ViewModels/ActionBar/Helper/StartOptionsToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Lib.Wpf.ConverterBase;
+using OQF.Bot.Contracts.Coordination;
+
+namespace OQF.PlayerVsBot.Visualization.ViewModels.ActionBar.Helper
+{
+    class StartOptionsToBoolConverter : GenericValueConverter<StartOptionsDisplayData, bool>
+    {
+        protected override bool Convert(StartOptionsDisplayData value, CultureInfo culture)
+        {
+            return value.PlayerStartingType != PlayerType.TopPlayer;
+        }
+
+        protected override StartOptionsDisplayData ConvertBack(bool value, CultureInfo culture)
+        {
+            return value ? new StartOptionsDisplayData(PlayerType.BottomPlayer,string.Empty) 
+                : new StartOptionsDisplayData(PlayerType.TopPlayer, string.Empty);
+        }
+    }
+}
